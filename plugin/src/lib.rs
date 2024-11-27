a8142cd7d66a0b8dd9d92ed7091a1fbc768c6082
Anders Pitman <&ers@apitman.com>
2024-11-27 01:13:40 -0700

    Remove tokio stuff

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -2,15 +2,8 @@ use extism_pdk::{debug,plugin_fn,FnResult};
 
 #[plugin_fn]
 pub extern "C" fn test() -> FnResult<()> {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-
-    rt.block_on(async {
-        debug!("here1");
-        tokio::time::sleep(std::time::Duration::from_millis(2000)).await;
-        debug!("here2");
-    });
+    debug!("{:?}", std::time::SystemTime::now());
+    std::thread::sleep(std::time::Duration::from_millis(2000));
+    debug!("{:?}", std::time::SystemTime::now());
     Ok(())
 }
